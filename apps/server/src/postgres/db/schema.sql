8bb15affa955109cae73bcdc99ef1b84a5527a93
Krascar <oscarmamani0079@gmail.com>
2026-09-18 18:01:25 -0500

    feat: agregar configuración de conexión a la base de datos y roles de usuario
    - Se configuró la conexión con PostgreSQL.
    - Se agregó `@types/pg` para los tipos de TypeScript de la librería `pg`.
    - Se agregaron las variables de entorno para la configuración de la base de datos.
    - Se agregó la carpeta connection en apps\server\src\postgres\connection con el archivo connection.ts en su interior
    - Se agregó  la carpeta db en apps\server\src\postgres\db con las query de la base de datos
    - Se agregó el archivo test-connection.ts en  apps\server\src\postgres\test-connection.ts para verificacion de la respuesta
    - Se agregó el archivo .example.env : apps\server\.example.env
    - Se agregó el archivo .gitignore : apps\server\.gitignore

---
+++ apps/server/src/postgres/db/schema.sql
@@ -0,0 +1,33 @@
+-- Crea el rol pgarmor_auditor
+CREATE ROLE pgarmor_auditor
+LOGIN;
+
+-- Dar acceso a la base de datos
+GRANT CONNECT ON DATABASE pgarmor
+TO pgarmor_auditor;
+
+-- Dar permisos de solo lectura
+GRANT pg_read_all_data
+TO pgarmor_auditor; 
+
+-- Verificar configuración del usuario
+SELECT
+    rolname,
+    rolcanlogin,
+    rolsuper,
+    rolcreatedb,
+    rolcreaterole,
+    rolbypassrls
+FROM pg_roles
+WHERE rolname = 'pgarmor_auditor';
+
+-- Verificar el rol 'pg_read_all_data'
+SELECT
+    pg_get_userbyid(member) AS usuario,
+    pg_get_userbyid(roleid) AS rol
+FROM pg_auth_members
+WHERE member = (
+    SELECT oid
+    FROM pg_roles
+    WHERE rolname = 'pgarmor_auditor'
+);
